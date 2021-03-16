500895d4c73e8445a6bdb4f6b106c50b120d90d8
shalookumari <78845430+shalookumari@users.noreply.github.com>
2021-03-16 17:35:24 +0530

    Add files via upload

---
+++ demo/src/main/java/com/ltts/demo/Controller/ProjectController.java
@@ -0,0 +1,25 @@
+package com.ltts.demo.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class ProjectController {
+
+@RequestMapping("/hii")
+public String firstMethod()
+{
+	return "hello SpringBoot";
+}
+	
+@RequestMapping("/")
+public ModelAndView secondMethod()
+{
+	return new ModelAndView("index");
+}
+	
+}
+
+
